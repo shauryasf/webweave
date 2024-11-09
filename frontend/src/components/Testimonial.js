1e97c4dcce67dac1de2ee37861aaa27b62d0cc74
poojan2305 <poojankothari190@gmail.com>
2024-11-09 19:35:25 +0530

    Improve readability

--- frontend/src/components/Testimonial.js
+++ frontend/src/components/Testimonial.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import builder from "../images/builder.png";
 
+// Testimonial component to be shown on the auth page
 const Testimonial = () => {
   return (
     <div className="flex flex-col items-center bg-[#026e53] text-white p-8 rounded-md shadow-lg w-100 h-100">
@@ -13,7 +14,6 @@ const Testimonial = () => {
         Using WebWeave has made creating a website easier than ever. Just drag, drop, and publish!
       </p>
 
-      {/* User Info */}
       <img src={builder} className="border-20"/>
 
       <div className="mt-6 bg-white text-black p-4 rounded-lg shadow-md flex items-center space-x-4 cursor-pointer">
