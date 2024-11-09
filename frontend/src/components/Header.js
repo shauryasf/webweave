858d86828a7fd754953a925d81944887e23d39f2
poojan2305 <poojankothari190@gmail.com>
2024-11-09 19:31:59 +0530

    Improve readability

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,22 @@
 import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
 function Header(){
+    // toggle to check if menu is open or not for responsiveness
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
     return (
         <header className="flex justify-between items-center pt-8 pb-8 pl-16 pr-16 relative">
-            <div className="text-2xl font-bold cursor-pointer">WebWeave</div>
+            <div className="text-2xl font-bold cursor-pointer" onClick={() => navigate("/")}>WebWeave</div>
             <nav className={`md:right-0 rounded p-2 ${menuOpen ? 'flex' : 'hidden'} md:block`}>
                 <ul className="flex flex-col md:flex-row gap-4 md:gap-6">
-                    <li><a href="#" className="text-gray-300 hover:text-white">Product</a></li>
-                    <li><a href="#" className="text-gray-300 hover:text-white">Features</a></li>
-                    <li><a href="#" className="text-gray-300 hover:text-white">Templates</a></li>
-                    <li><a href="#" className="text-gray-300 hover:text-white">Pricing</a></li>
-                    <li><a href="#" className="text-gray-300 hover:text-white">FAQ</a></li>
+                    <li><a className="text-gray-300 hover:text-white cursor-pointer" onClick={() => navigate("/")}>Home</a></li>
+                    <li><a className="text-gray-300 hover:text-white cursor-pointer" onClick={() => navigate("/#home-features")}>Features</a></li>
+                    <li><a className="text-gray-300 hover:text-white cursor-pointer" onClick={() => navigate("/auth")}>Login</a></li>
                 </ul>
             </nav>
             <button className="flex flex-col md:hidden" onClick={toggleMenu}>
